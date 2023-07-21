cf6868b3d68399f67ddecc53811c11e71509891f
rakhaharis08 <rakhaharis080102@gmail.com>
2023-07-21 21:14:06 +0700

    BETa

--- src/configs/verify.js
+++ src/configs/verify.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db_edom',
+    host                : 'bsvvvqfjx0qfkq4sewhu-mysql.services.clever-cloud.com',
+    user                : 'u9gceim0rieilj5t',
+    password            : 'PQXuecc7MTvgvlJHp4vD',
+    database            : 'bsvvvqfjx0qfkq4sewhu'
 };
 
 const connection = mysql.createConnection(dbConfig);
