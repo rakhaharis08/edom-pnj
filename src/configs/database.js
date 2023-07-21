beadcd9cf92d6126bc1824294bf2c5c9639295b2
rakhaharis08 <rakhaharis080102@gmail.com>
2023-07-21 20:58:01 +0700

    BETa

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,6 +2,6 @@ module.exports = {
     multipleStatements  : true,
     host                : 'bsvvvqfjx0qfkq4sewhu-mysql.services.clever-cloud.com',
     user                : 'u9gceim0rieilj5t',
-    password            : 'u9gceim0rieilj5t',
+    password            : 'PQXuecc7MTvgvlJHp4vD',
     database            : 'bsvvvqfjx0qfkq4sewhu'
 };
